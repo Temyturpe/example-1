4e690b2ebf896ab58e51bb5601a6fc3e3ff09138
Temitope <themyturpeokesanya@gmail.com>
2023-07-19 19:22:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,471 @@
+'use strict';
+// let myName = "Temitope";
+// if (myName === "Temitope") alert("Temmy is a frontend engineer");
+
+// let myAge = 19;
+// console.log(myAge);
+
+// let myName = 'lovelyn';
+// let Ages = 19 + 30 + 2 + 9;
+
+// console.log(myName);
+// console.log(Ages);
+
+// let age = 30;
+// let myName = "temitope";
+// let isAnEngineer = true;
+// let favFood;
+
+// console.log(typeof age);
+// console.log(typeof myName);
+// console.log(typeof isAnEngineer);
+// console.log(typeof favFood);
+
+// var temmieSchool = "Lagos state university";
+// console.log(temmieSchool);
+
+// let age = 18;
+// age = 19;
+// age = 20;
+// console.log(age);
+
+// const temitope = true;
+// temitope = false;
+// console.log(temitope);
+
+// const currentYear = 2023;
+// const birthYear = 1998;
+// const currentAge = currentYear - birthYear;
+
+// console.log(currentAge);
+
+// const answer = 12 * 3 / (23 - 1);
+// console.log(answer);
+// const totalMassMales = 30 + 30 + 60 + 42;
+// const totalNumberOfMale = 4;
+// const totalMassFemales = 30 + 30 + 60 + 42 + 50;
+// const totalNumberOfFemale = 5;
+
+// const avgBodyMassOfMales = totalMassMales / totalNumberOfMale;
+// const avgBodyMassOfFemales = totalMassFemales / totalNumberOfFemale;
+
+// console.log(avgBodyMassOfMales);
+// console.log(avgBodyMassOfFemales);
+
+// console.log(avgBodyMassOfMales < avgBodyMassOfFemales);
+
+
+
+// const BMI = bodyMass / height ** 2;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markBmi = markMass / markHeight ** 2;
+// const johnBmi = johnMass / johnHeight ** 2;
+
+// const isMarkBMIHeigher = markBmi > johnBmi;
+// console.log(markBmi);
+// console.log(johnBmi);
+// console.log(isMarkBMIHeigher);
+
+// const chikaName = "Adedoyin Deborah";
+// const chikaSchool = "Axia Africa";
+// const chikaExperience = 2;
+// const chikaFavFood = "Eba";
+
+// console.log(`${chikaName} is a student of ${chikaSchool}. She has ${chikaExperience} experience in frontend development and her favourite food is ${chikaFavFood}.`);
+
+// console.log(chikaName + ' is a student of ' + chikaSchool + '. She has ' + chikaExperience + ' experience in frontend development and her favourite food is ' + chikaFavFood );
+
+
+
+
+// const BMI = bodyMass / height ** 2;
+// const markMass = 92;
+// const markHeight = 1.95;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markBmi = markMass / markHeight ** 2;
+// const johnBmi = johnMass / johnHeight ** 2;
+
+// const isMarkBMIHeigher = markBmi > johnBmi;
+// console.log(markBmi);
+// console.log(johnBmi);
+// console.log(isMarkBMIHeigher);
+
+// if(isMarkBMIHeigher){
+//     console.log(`Mark has a higher BMI of ${markBmi}`);
+// } else if(markBmi === johnBmi){
+//     console.log(`Mark and John have the same BMI`);
+// } else{
+//     console.log(`John has a higher BMI of ${johnBmi}`);
+// } 
+
+// const myName = Number('Temitope');
+// const myExperience = 0;
+// console.log(myName, myExperience);
+// console.log(typeof myName);
+// console.log(typeof myExperience);
+// console.log(String(myExperience));
+
+// const answer = 44 + 22 + "44";
+// console.log(answer);
+
+// 0, "", undefined, null, NaN
+// console.log(Boolean(myName));
+
+// const myAge = "20"; //string
+// const nextAge = 20; //number
+// const totalAge = myAge + nextAge;
+
+// if(myAge === nextAge){
+//     prompt("good to go!");
+// } else{
+//     prompt("computer is confused");
+// }
+
+// minimum requirment for a job is to be above 18years and completed nysc and lives in lagos
+// AND ( && ), OR ( || ) and NOT ( ! );
+
+
+// const blessingAge = true;
+// const blessingNysc = false;
+// const blessingLagos = false;
+
+// if(!blessingLagos, !blessingAge, !blessingLagos){
+//     console.log("congratulations, we want to work with you");
+// }else{
+//     console.log("you dont qualify");
+// }
+
+
+// blessingAge && blessingNysc && blessingLagos ? console.log("congratulations, we want to work with you") : console.log("you dont qualify");
+
+// const day = 'thursday';
+
+// switch(day) {
+//     case 'monday':
+//         console.log('check my email');
+//         console.log('brush my teeth');
+//         console.log('check week schedule');
+//         console.log('eat my food');
+//         console.log('code');
+//         console.log('read and socialize');
+//         console.log('review class');
+//         console.log('eat and do other stuff');
+//         break;
+//      case 'tuesday':
+//         console.log('check my email');
+//         console.log('brush my teeth');
+//         console.log('eat my food');
+//         console.log('code');
+//         console.log('read and socialize');
+//         console.log('eat and do other stuff');  
+//         break;
+//      case 'wednesday':
+//         console.log('check my email');
+//         console.log('brush my teeth');
+//         console.log('check week schedule');
+//         break;
+//      case 'thursday':
+//         console.log('review class');
+//         console.log('eat and do other stuff');
+//         break;
+//      case 'friday':
+//         console.log('review class');
+//         console.log('eat and do other stuff');
+//         console.log('review class');
+//         console.log('eat and do other stuff');
+//         break;
+//      case 'saturday':
+//             console.log('check my email');
+//             console.log('brush my teeth');
+//             console.log('eat my food');
+//             console.log('code');
+//             console.log('read and socialize');
+//             console.log('eat and do other stuff');  
+//             break;
+//      case  'sunday':
+//             console.log('check my email');
+//             console.log('brush my teeth');
+//             console.log('check week schedule');
+//          break;
+//     default:
+//         console.log('not a day');
+
+// }
+
+// if(day === 'monday'){
+//     console.log('check my email');
+//     console.log('brush my teeth');
+//     console.log('check week schedule');
+//     console.log('eat my food');
+//     console.log('code');
+//     console.log('read and socialize');
+//     console.log('review class');
+//     console.log('eat and do other stuff');
+// } else if(day === 'tuesday'){
+//     console.log('check my email');
+//     console.log('brush my teeth');
+//     console.log('eat my food');
+//     console.log('code');
+//     console.log('read and socialize');
+//     console.log('eat and do other stuff');  
+// }else if(day === 'wednesday'){
+//     console.log('check my email');
+//     console.log('brush my teeth');
+//     console.log('eat my food');
+//     console.log('code');
+//     console.log('read and socialize');
+//     console.log('eat and do other stuff');  
+// }else{
+//     console.log('do nothing today');
+// }
+
+// function
+
+// function mySelf() {
+//     console.log("Temitope");
+// }
+// f. declaration
+// const studentData1 = student("Doyin", "Frontend development");
+// console.log(studentData1);
+// function student(studentName, studentCourse) {
+//     const details = `${studentName} is a student at axia africa. Taking ${studentCourse}.`;
+//     return  details;
+// }
+// student("Doyin", "Frontend development");
+
+// const studentData2 = student("Joel", "Data analysis");
+// console.log(studentData2);
+
+// f. expression
+// const student2 = function (studentName, studentCourse) {
+//     const details2 = `${studentName} is a student at axia africa. Taking ${studentCourse}.`;
+//     return  details2;
+// }
+// const studentData3 = student2("lovelyn", "Product Design");
+// console.log(studentData3);
+//  arrow function
+// const student3 = (studentName, studentCourse) => `${studentName} is a student at axia africa. Taking ${studentCourse}.`;
+// const studentData4 = student3("finbarr", "product marketing");
+// console.log(studentData4);
+
+
+
+// function in a function
+
+// fruit cutter function
+// const fruitCutter = fruitNumber => fruitNumber * 10;
+
+// fruitJuicer
+// const fruitJuicer = function (apples, mango, orange){
+//     const appleNumber = fruitCutter(apples);
+//     const mangoNumber = fruitCutter(mango);
+//     const orangeNumber = fruitCutter(orange);
+
+//     const fruitJuice = `Juice made with ${appleNumber} pieces of apples and ${mangoNumber} pieces of mangos and ${orangeNumber} pieces of orange.`;
+//  return fruitJuice;
+// }
+
+// console.log(fruitJuicer(2, 1, 4));
+
+// example
+// age Calculator
+// const currentAge = birthYear => 2023 - birthYear;
+
+//alcohol caution machine
+// const ageToTakeAlcohol = function (birthYear, firstName) {
+//     const age = currentAge(birthYear);
+//     const eligibleAge = age;
+//     const remianingAge = 18 - age;
+
+//     if (age >= 18 ) {
+//         console.log(`${firstName} is old enough to take alcohol`);
+//         return remianingAge;
+//     } else {
+//         console.log(`${firstName}!!!...... you have ${remianingAge} years to take alcohol...!!!`);
+//         return remianingAge;
+//     }
+    
+// }
+// console.log(ageToTakeAlcohol(2006, "chika"));
+// console.log(ageToTakeAlcohol(1994, "finbarr"));
+
+// arrays
+// const frontendStnd = ['doyin', 'blessing', 'chika', 12, 'lovelyn']; 
+// const courses = new Array ('frontend', 'product', 'data');
+// console.log(frontendStnd);
+// console.log(frontendStnd[1]);
+// console.log(frontendStnd[3]);
+// console.log(frontendStnd[frontendStnd.length - 2]);
+
+// frontendStnd[2] = 'dimeji';
+// console.log(frontendStnd);
+
+
+// const currentYear = 2023;
+// const birthYear = 1999; 
+// const firstName = 'dimeji';
+// const lastName = 'Balogun'
+// const sex = 'male';
+// const colleagues = ['doyin', 'blessing', 'finbarr'];
+// const bestFood = 'rice';
+
+// const dimeji = [firstName, 'male', ['doyin', 'blessing', 'finbarr'], 2023 - 1999];
+// // console.log(dimeji);
+// dimeji.push(bestFood); /// adds to the last position
+// console.log(dimeji);
+// dimeji.unshift(lastName); ///adds to the first position
+// console.log(dimeji);
+// dimeji.pop(); /// remove last value
+// console.log(dimeji);
+// dimeji.shift(); ///remove first value 
+// console.log(dimeji);
+
+//indexOf
+// console.log(dimeji.indexOf(colleagues));
+// console.log(dimeji.includes(location));
+
+// if (dimeji.includes(location)){
+//     console.log('Dimeji is in lagos');
+// }else{
+//     console.log('dimeji please add your location');
+// }
+
+// objects
+// const doyin = {
+//     firstName: 'dimeji',
+//     birthYear: 2011,
+//     lastName: 'Balogun',
+//     sex: 'male',
+//     colleagues: ['doyin', 'blessing', 'finbarr'],
+//     bestFood: 'rice',
+    // currentAge: function(birthYear){
+    //     return 2023 - birthYear;
+    // },
+    // currentAge: function(){
+    //     return 2023 - this.birthYear;
+    // }
+//    currentAgeFuct: function(){
+//     this.age = 2023 - this.birthYear;
+//     return this.age;
+//    }
+
+    
+
+// };
+// console.log(doyin.age);
+// console.log(doyin.currentAgeFuct(birthYear));
+
+// const blessingPushUps = 20;
+// if(blessingPushUps < 20){
+//     console.log('push more');
+// }else{
+//     console.log('you can rest now');
+// }
+
+// for(let pushUps = 1 ; pushUps <= 8; pushUps ++){
+//     console.log(`push more, you just did ${pushUps} pushup`);
+// }
+
+// let pushUps = 1;
+// while (pushUps <= 8) {
+//     console.log(`push more, you just did ${pushUps} pushup`);
+//     pushUps ++;
+// }
+
+
+// const dimeji2 = [firstName, 'male', ['doyin', 'blessing', 'finbarr'], 2023 - 1999, true];
+// for(let i = 0; i < dimeji.length; i ++ ){
+//     console.log(dimeji2[i]);
+// }
+
+// const birthYears = [1999, 2001, 1990, 2003];
+// const ages = [];
+
+// for(let i = 0; i < birthYears.length; i ++){
+//     ages.push(currentYear - birthYears[i])
+// }
+// console.log(ages);
+
+// console.log(dimejiObject.currentAge(birthYear));
+
+// console.log(dimejiObject)
+//dot method
+// console.log(dimejiObject.sex);
+//bracket method
+// console.log(dimejiObject['sex']);
+
+// const aboutDimeji = prompt('what do you want to know? firstName. birthYear, lastName, sex, colleagues, bestFood');
+
+// if (dimejiObject[aboutDimeji]) {
+//     console.log(dimejiObject[aboutDimeji]);
+// }else{
+//     console.log('i dont share such info with strangers');
+// }
+// console.log(dimejiObject['location']);
+
+// //////////DOM MANIPULATION
+
+//element selection
+// const grandChild = document.getElementById('grandChild2').innerText;
+// console.log(grandChild);
+
+// const element3 = document.getElementsByClassName('grandchild');
+// console.log(element3[2].innerText);
+
+// const parag = document.getElementsByTagName('p');
+// console.log(parag[0].innerText);
+
+// const grandkids = document.querySelector('p');
+// console.log(grandkids.innerText);
+
+//element styling
+const buttons = document.querySelectorAll('.btn');
+const btnStyle = document.getElementsByTagName('button');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+
+const openPopup = function(){
+    popup.classList.remove('hide-popup');
+    
+    
+}
+const closePopup = function(){
+    popup.classList.add('hide-popup');
+}
+
+for (let i = 0; i < buttons.length; i++){
+   buttons[i].addEventListener('click', openPopup);
+}
+
+close.addEventListener('click', closePopup);
+
+// button.addEventListener('click', function () {
+//     button.classList.add('btnstyle');
+// })
+// button.addEventListener('click', function () {
+//     button.classList.remove('btnstyle')
+// })
+// button.addEventListener('click', function(){
+//     button.classList.toggle('btnstyle');
+//     popup.classList.toggle('hide-popup');
+// })
+
+// if(button.classList.contains('btnstyle')){
+//   button.innerText = 'Close me';
+// }else{
+//     button.innerText = 'Click me';
+// };
+
+
+
+
+
+
+
+
